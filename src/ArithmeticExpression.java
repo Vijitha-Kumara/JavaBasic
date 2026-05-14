0304077c74ab7088c7cc3c2ccdea7a7c20329552
Vijitha Kumara Ranamuka <vijitha.kumara.wu@gmail.com>
2026-05-14 11:31:48 +0530

    Feat: Create classes for Java ArithmeticExpression

---
+++ src/ArithmeticExpression.java
@@ -0,0 +1,16 @@
+public interface ArithmeticExpression  {
+     public static void main(String[] args) {
+         double result =(double)10 / (double)3;
+         System.out.println(result);
+         int x =1;
+         x+=3;
+         x-=1;
+         x/=3;
+         x*=10;
+         x%=10;
+         int y =(10+3)*2;
+         System.out.println(x);
+         System.out.println(y);
+
+}
+}
