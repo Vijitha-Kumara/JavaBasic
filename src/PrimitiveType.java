c2a9bcf2720a1e11435e1e8bd5d1f96cee1b451e
Vijitha Kumara Ranamuka <vijitha.kumara.wu@gmail.com>
2026-05-14 10:24:23 +0530

    Feat: Create classes for Java primitive types

---
+++ src/PrimitiveType.java
@@ -0,0 +1,17 @@
+public class PrimitiveType {
+    public static void main(String[] args) {
+         byte age = 30;
+         long viewCount =3_123_145_455L;
+         double salary = 999.8;
+         float commission = 50;
+         char gender = 'M';
+          boolean isEdit = false;
+
+
+        System.out.println(viewCount);
+        System.out.println(salary);
+        System.out.println(commission);
+        System.out.println(gender);
+        System.out.println(isEdit);
+    }
+}
